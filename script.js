385517c35a7b1bb46730c598381cf36f4be10f09
Shreyash Hiroji <shreyashhiroji4@gmail.com>
2026-09-23 11:18:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const tapMessage = document.getElementById("tapMessage");
 // TEST MODE
 // Change to false after everything works.
 
-const TEST_MODE = true;
+const TEST_MODE = false;
 
 const birthday = TEST_MODE
     ? Date.now() + 10000
